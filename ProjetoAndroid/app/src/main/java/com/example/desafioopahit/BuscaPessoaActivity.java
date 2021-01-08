b94b5384674c0d1ceffd4d636b3eabe51b49b13b
Filipe Dias <filipeandrade41@outlook.com>
2021-01-08 11:55:14 -0300

    Adicionando Design da segunda tela, implementando gif e ligação entre as duas primeiras telas

---
+++ ProjetoAndroid/app/src/main/java/com/example/desafioopahit/BuscaPessoaActivity.java
@@ -0,0 +1,14 @@
+package com.example.desafioopahit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BuscaPessoaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_busca_pessoa);
+    }
+}
--- ProjetoAndroid/app/src/main/java/com/example/desafioopahit/MainActivity.java
+++ ProjetoAndroid/app/src/main/java/com/example/desafioopahit/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.desafioopahit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void btnPeopleIntent(View view){
+        Intent intent = new Intent(getApplicationContext(), BuscaPessoaActivity.class);
+        startActivity(intent);
+    }
+
 }
